ff3db2a9e282fff792d15921c6dafd05d0a08fef
Bogdan Nicula <bogdan@nicula.net>
2018-10-05 10:00:01 +0200

    Bring in line with GeometryService

--- swhv-propagation/propaserv/PropagationService/service.py
+++ swhv-propagation/propaserv/PropagationService/service.py
@@ -1,5 +1,6 @@
+
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 from spyne.application import Application
 from spyne.decorator import rpc
@@ -7,35 +8,34 @@ from spyne.service import ServiceBase
 from spyne.model.primitive import AnyDict, Integer, Float, String
 from spyne import Array
 
-from spyne.protocol.http import HttpRpc
 from spyne.protocol.json import JsonDocument
-from spyne.protocol.msgpack import MessagePackDocument
 
 from spyne.server.wsgi import WsgiApplication
 
 #####
 
-class SolarSystemPropagationService(ServiceBase):
+class PropagationService(Service):
     @rpc(Array(Integer), Integer,
          _returns=Array(Integer))
     def time_shift(ctx, needs_shift, shift):
         for i in range(len(needs_shift)):
             needs_shift[i] = needs_shift[i] + shift
         return needs_shift
-
 #####
 
 def propagation_service(fcgi=True):
     if fcgi is False:
         def _on_method_return_object(ctx):
             ctx.transport.resp_headers['Access-Control-Allow-Origin'] = "*"
+            ctx.transport.resp_headers['Cache-Control'] = "public,max-age=86400" # tbd
 
-        SolarSystemPropagationService.event_manager.add_listener('method_return_object',
-                                                              _on_method_return_object)
+        PropagationService.event_manager.add_listener('method_return_object',
+                                                      _on_method_return_object)
 
     tns = 'sidc.service.propagation'
 
-    json = Application([SolarSystemPropagationService], tns=tns,
+    json = Application([PropagationService], tns=tns,
                        in_protocol=JsonDocument(validator='soft'),
                        out_protocol=JsonDocument())
+
     return WsgiApplication(json)
