bdbb547cce7a10ccdb006c601cc3ece6c924afdc
Bogdan Nicula <bogdan@nicula.net>
2018-10-05 18:37:31 +0200

    Manual connection to GeometryService

--- service/PropagationService/service.py
+++ service/PropagationService/service.py
@@ -3,24 +3,46 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 from spyne.application import Application
-from spyne.decorator import rpc
+from spyne.decorator import srpc
 from spyne.service import Service
-from spyne.model.primitive import Integer
-from spyne import Array
+from spyne.model.primitive import AnyDict, Float, Unicode
 
+from spyne.protocol.http import HttpRpc
 from spyne.protocol.json import JsonDocument
 
 from spyne.server.wsgi import WsgiApplication
 
+from spyne.util.six.moves.urllib.request import Request, urlopen
+from spyne.util.six.moves.urllib.error import HTTPError
+
 #####
 
+server = 'http://127.0.0.1:7789/json/position?'
+observer = 'SUN'
+target = 'SOHO'
+ref = 'HEEQ'
+
 class PropagationService(Service):
-    @rpc(Array(Integer), Integer,
-         _returns=Array(Integer))
-    def time_shift(ctx, needs_shift, shift):
-        for i in range(len(needs_shift)):
-            needs_shift[i] = needs_shift[i] + shift
-        return needs_shift
+    @srpc(Unicode(min_occurs=1), Unicode(min_occurs=1),
+          Unicode(min_occurs=0), Float(min_occurs=0),
+          _returns=AnyDict)
+    def propagation(name, utc, utc_end, deltat):
+        utc_end_req = '' if utc_end is None else '&utc_end=' + utc_end
+        deltat_req = '' if deltat is None else '&deltat=' + deltat
+
+        url = server + 'utc=' + utc + '&observer=' + observer + '&target=' + target + '&ref=' + ref + utc_end_req + deltat_req
+        request = Request(url)
+        code = 200
+
+        try:
+            response = urlopen(request)
+            in_string = response.read().decode('utf-8')
+        except HTTPError as e:
+            code = e.code
+            in_string = e.read().decode('utf-8')
+
+        return {'geometry': in_string}
+
 #####
 
 def propagation_service(fcgi=True):
@@ -32,10 +54,8 @@ def propagation_service(fcgi=True):
         PropagationService.event_manager.add_listener('method_return_object',
                                                       _on_method_return_object)
 
-    tns = 'swhv.service.propagation'
-
-    json = Application([PropagationService], tns=tns,
-                       in_protocol=JsonDocument(validator='soft'),
+    json = Application([PropagationService], tns='swhv.service.propagation',
+                       in_protocol=HttpRpc(validator='soft'),
                        out_protocol=JsonDocument())
 
     return WsgiApplication(json)
